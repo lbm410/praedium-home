6d86ca3d9b178972aa71a2e4073724f687021f94
Lucas Barrientos <lbm410@inlumine.ual.es>
2024-12-27 20:14:25 +0100

    New features

--- script.js
+++ script.js
@@ -9,3 +9,44 @@ function toggleMenu() {
 
 // Event listener para el botón de hamburguesa
 menuToggle.addEventListener('click', toggleMenu);
+
+    // Obtener referencia al elemento de video
+    var myVideo = document.getElementById("myVideo");
+
+    // Función para pausar/reanudar al hacer clic en el video
+    function togglePlayPause() {
+        if (myVideo.paused) {
+            myVideo.play();
+        } else {
+            myVideo.pause();
+        }
+    }
+
+    // Asignar la función al evento clic en el video
+    myVideo.addEventListener("click", togglePlayPause);
+
+    let lastScrollY = window.scrollY;
+const header = document.querySelector('.header');
+
+// Altura desde la cual empieza a funcionar el comportamiento
+const activationHeight = 50; 
+
+// Escucha el evento de desplazamiento
+window.addEventListener('scroll', () => {
+    // Solo aplica en dispositivos móviles
+    if (window.innerWidth <= 768) {
+        if (window.scrollY <= activationHeight) {
+            // Si estamos en la parte superior, el header se queda fijo
+            header.style.transform = 'translateY(0)';
+        } else {
+            if (window.scrollY > lastScrollY) {
+                // Si el usuario está desplazándose hacia abajo, oculta el header
+                header.style.transform = 'translateY(-100%)';
+            } else {
+                // Si el usuario está desplazándose hacia arriba, muestra el header
+                header.style.transform = 'translateY(0)';
+            }
+        }
+        lastScrollY = window.scrollY;
+    }
+});
